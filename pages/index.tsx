d63f892fb4687f5e6bfb5ce746ef0db50d0cf894
Edgar Coime <59620774+edgarcoime@users.noreply.github.com>
2022-02-19 19:01:11 -0800

    feat: add google signin functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
-import { ReactElement } from 'react'
-import Link from 'next/link';
-import PublicLayout from '../lib/components/layouts/PublicLayout'
-import { Container, Center, TextInput, PasswordInput, Button, Space, Text, Image } from '@mantine/core'
+import { Container, Center, TextInput, PasswordInput, Button, Space, Text, Image, Group } from '@mantine/core';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { useAuth } from 'reactfire';
 
 const Home = () => {
+  const provider = new GoogleAuthProvider();
+  const fbAuth = useAuth();
+
+  const signinThroughGoogle = () => {
+    console.log("Signin through google");
+    signInWithPopup(fbAuth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const user = result.user;
+      console.log(user);
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      console.log(credential);
+    })
+  }
+
   return (
     <div style={{
       display: "flex", 
@@ -39,23 +57,42 @@ const Home = () => {
             required
           />
           <Space h="md" />
-          <Button 
-            component="a" 
-            href="/hello" 
-            variant="white" 
-            size="xs" 
-            radius="sm"
-            styles={(theme) => ({
-              root: {
-                borderColor: theme.colors.blue[8]
-              }
-            })}>
-            Sign In
-          </Button>
+          <Group direction='column' grow>
+            <Button 
+              variant="white" 
+              size="xs" 
+              radius="sm"
+              styles={(theme) => ({
+                root: {
+                  borderColor: theme.colors.blue[8]
+                }
+              })}>
+              Sign In
+            </Button>
+            <Button
+              variant='white'
+              size='xs'
+              radius="sm"
+              styles={(theme) => ({
+                root: {
+                  borderColor: theme.colors.blue[8]
+                }
+              })}
+              onClick={() => signinThroughGoogle()}
+            >
+              Google Sign In
+            </Button>
+
+          </Group>
           <Space h="xs" />
           <Text color="dimmed" size="xs">
-            Don't have an account with us yet?&nbsp;
-            <Text variant="link" component="a" size="xs">Sign up here!</Text>
+            Don&apos;t have an account with us yet?&nbsp;
+            <Text 
+              variant="link" 
+              component="a" 
+              size="xs" 
+              onClick={() => console.log("hello")}
+            >Sign up here!</Text>
           </Text>
         </Container>
       </Center>
